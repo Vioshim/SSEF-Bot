a27aaa1d19f4c2bf4e84c73ea144fd5e73637c25
Kevin Rangel <krangelj@unicartagena.edu.co>
2024-01-17 21:57:41 -0500

    Fix

--- cogs/submission/__init__.py
+++ cogs/submission/__init__.py
@@ -71,20 +71,12 @@ class Submission(commands.Cog):
             and (ctx.guild and ctx.guild.get_member(o.user_id))
         ]
 
-        if len(text) > 2 and (
-            result := (
-                process.extractOne(
-                    text,
-                    ocs,
-                    processor=lambda x: x.oc_name if isinstance(x, Character) else x,
-                    score_cutoff=90,
-                )
-                or process.extractOne(
-                    text,
-                    ocs,
-                    processor=lambda x: x.name if isinstance(x, Character) else x,
-                    score_cutoff=90,
-                )
+        if len(text) >= 2 and (
+            result := process.extractOne(
+                text,
+                ocs,
+                processor=lambda x: x.oc_name if isinstance(x, Character) else x,
+                score_cutoff=90,
             )
         ):
             return await ctx.invoke(self.read, oc=result[0])
